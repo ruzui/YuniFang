0ec2cc23a22c5936b6cf00e99903b611c553aa10
dell <1727226822@>
2017-04-15 10:12:30 +0800

    项目第四次提交

--- app/src/main/java/myapplication888/bwie/com/yunifang/Fragment/FlFragment.java
+++ app/src/main/java/myapplication888/bwie/com/yunifang/Fragment/FlFragment.java
@@ -1,23 +1,38 @@
 package myapplication888.bwie.com.yunifang.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import myapplication888.bwie.com.yunifang.R;
+import myapplication888.bwie.com.yunifang.dateActivity.mianno;
 
 /**
  * Created by dell on 2017/4/12.
  */
 public class FlFragment extends Fragment {
+    private ImageView mian;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View inflate = View.inflate(getActivity(), R.layout.flfragment, null);
-
+        mian = (ImageView) inflate.findViewById(R.id.mianmo);
+        onClickListener();
         return  inflate;
+    }
+
+    private void onClickListener() {
+        mian.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(getActivity(),mianno.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/myapplication888/bwie/com/yunifang/dateActivity/mianno_bean.java
@@ -0,0 +1,149 @@
+package myapplication888.bwie.com.yunifang.dateActivity;
+
+import java.util.List;
+
+/**
+ * Created by dell on 2017/4/14.
+ */
+public class mianno_bean {
+
+    /**
+     * code : 200
+     * data : [{"efficacy":"镇店之宝 美白爆款","goods_img":"https://image.yunifang.com/yunifang/images/goods/121/goods_img/170301091610311632161123479.jpg","goods_name":"镇店之宝丨美白嫩肤面膜7片","id":"121","is_allow_credit":false,"is_coupon_allowed":true,"market_price":99,"sales_volume":122443,"shop_price":49.9,"sort":0},{"efficacy":"以黑吸黑 净透亮肤","goods_img":"http://image.hmeili.com/yunifang/images/goods/389/goods_img/16081909368531961221339216.jpg","goods_name":"清爽平衡矿物蚕丝面膜黑面膜21片","id":"389","is_allow_credit":false,"is_coupon_allowed":true,"market_price":297,"sales_volume":101959,"shop_price":99.9,"sort":0},{"efficacy":"水嫩舒爽 鲜活亮颜","goods_img":"http://image.hmeili.com/yunifang/images/goods/85/goods_img/160819085747012099748482408.jpg","goods_name":"果味鲜饮|水果鲜润亮肤补水面膜套装17片","id":"85","is_allow_credit":false,"is_coupon_allowed":false,"market_price":240,"sales_volume":92390,"shop_price":59.9,"sort":0},{"efficacy":"水润充盈 鲜嫩少女肌","goods_img":"http://image.hmeili.com/yunifang/images/goods/684/goods_img/160819095063413908186337181.jpg","goods_name":"解救肌渴丨花粹水润面膜套装10片","id":"684","is_allow_credit":false,"is_coupon_allowed":false,"market_price":139,"sales_volume":85146,"shop_price":29.9,"sort":0},{"efficacy":"热销黑膜 净透亮肤","goods_img":"http://image.hmeili.com/yunifang/images/goods/772/goods_img/1608191429278479187604212.jpg","goods_name":"清爽亮颜黑面膜套装21片","id":"772","is_allow_credit":false,"is_coupon_allowed":true,"market_price":297,"sales_volume":51607,"shop_price":99.9,"sort":0},{"efficacy":"爆款面膜 明星同款","goods_img":"https://image.yunifang.com/yunifang/images/goods/8/goods_img/1703060954389257968293129.jpg","goods_name":"镇店之宝|美白嫩肤面膜20片","id":"8","is_allow_credit":false,"is_coupon_allowed":true,"market_price":298,"sales_volume":26855,"shop_price":119.9,"sort":0},{"efficacy":"深度保养 补水提亮","goods_img":"http://image.hmeili.com/yunifang/images/goods/83/goods_img/16081908576425344499831215.jpg","goods_name":"黑玫瑰矿物蚕丝面膜7片","id":"83","is_allow_credit":false,"is_coupon_allowed":true,"market_price":139,"sales_volume":26513,"shop_price":119,"sort":0},{"efficacy":"吸黑焕彩 补水保湿","goods_img":"http://image.hmeili.com/yunifang/images/goods/428/goods_img/160819094034113421009937866.jpg","goods_name":"多彩水润亮颜蚕丝面膜套装21片","id":"428","is_allow_credit":false,"is_coupon_allowed":true,"market_price":270.9,"sales_volume":23204,"shop_price":79.9,"sort":0},{"efficacy":"补水保湿 舒缓修护","goods_img":"http://image.hmeili.com/yunifang/images/goods/257/goods_img/16081909269443843637779188.jpg","goods_name":"薰衣草蚕丝面膜7片","id":"257","is_allow_credit":false,"is_coupon_allowed":true,"market_price":109,"sales_volume":21345,"shop_price":89,"sort":0},{"efficacy":"葡萄鲜饮 净透亮肤","goods_img":"http://image.hmeili.com/yunifang/images/goods/593/goods_img/1608190945359698973439364.jpg","goods_name":"热销黑膜丨葡萄籽琉璃亮颜黑面膜21片","id":"593","is_allow_credit":false,"is_coupon_allowed":true,"market_price":297,"sales_volume":18253,"shop_price":99.9,"sort":0},{"efficacy":"清透滋养 弹嫩紧致","goods_img":"http://image.hmeili.com/yunifang/images/goods/20/goods_img/160819084923710469247382812.jpg","goods_name":"清润紧致蚕丝面膜套装21片","id":"20","is_allow_credit":false,"is_coupon_allowed":true,"market_price":307,"sales_volume":15530,"shop_price":109.9,"sort":0},{"efficacy":"补水亮肤 缤纷水嫩","goods_img":"http://image.hmeili.com/yunifang/images/goods/400/goods_img/16081909375548938767322654.jpg","goods_name":"缤纷鲜润矿物蚕丝面膜套装20片","id":"400","is_allow_credit":false,"is_coupon_allowed":true,"market_price":279.9,"sales_volume":13265,"shop_price":79.9,"sort":0},{"efficacy":"补水保湿 提亮肤色","goods_img":"http://image.hmeili.com/yunifang/images/goods/745/goods_img/160819095890910677609745693.jpg","goods_name":"红石榴矿物蚕丝面膜7片","id":"745","is_allow_credit":false,"is_coupon_allowed":true,"market_price":99,"sales_volume":11594,"shop_price":79,"sort":0},{"efficacy":"平衡水油 清爽净透","goods_img":"http://image.hmeili.com/yunifang/images/goods/325/goods_img/160819142666914591033736475.jpg","goods_name":"竹萃清爽蚕丝面膜7片","id":"325","is_allow_credit":false,"is_coupon_allowed":true,"market_price":99,"sales_volume":9390,"shop_price":79,"sort":0},{"efficacy":"清洁补水 保湿提亮","goods_img":"http://image.hmeili.com/yunifang/images/goods/1249/goods_img/170104100411616941119547437.jpg","goods_name":"口碑推荐|盈透柔肤黑膜组合装42片","id":"1249","is_allow_credit":false,"is_coupon_allowed":true,"market_price":594,"sales_volume":8794,"shop_price":199.9,"sort":0},{"efficacy":"补水保湿 清爽控油","goods_img":"http://image.hmeili.com/yunifang/images/goods/161/goods_img/16081909077546170320476224.jpg","goods_name":"男士补水活力矿物蚕丝7片 今日送7片共14片","id":"161","is_allow_credit":false,"is_coupon_allowed":true,"market_price":99,"sales_volume":8171,"shop_price":59.9,"sort":0},{"efficacy":"新品组合 鲜果养肤","goods_img":"http://image.hmeili.com/yunifang/images/goods/708/goods_img/16082615049413682815961884.jpg","goods_name":"水果护肤力量丨鲜果缤纷黑白面膜套装42片","id":"708","is_allow_credit":false,"is_coupon_allowed":true,"market_price":594,"sales_volume":7146,"shop_price":209.9,"sort":0},{"efficacy":"补水保湿 清爽控油","goods_img":"http://image.hmeili.com/yunifang/images/goods/319/goods_img/16081909273161845841818485.jpg","goods_name":"男士补水活力矿物蚕丝21片","id":"319","is_allow_credit":false,"is_coupon_allowed":true,"market_price":297,"sales_volume":6294,"shop_price":79.9,"sort":0},{"efficacy":"葡萄鲜饮 净透亮肤","goods_img":"http://image.hmeili.com/yunifang/images/goods/622/goods_img/160819094698413173347474163.jpg","goods_name":"葡萄籽琉璃亮颜黑面膜7片","id":"622","is_allow_credit":false,"is_coupon_allowed":true,"market_price":99,"sales_volume":6198,"shop_price":79,"sort":0},{"efficacy":"提亮肤色 修护肌肤","goods_img":"http://image.hmeili.com/yunifang/images/goods/921/goods_img/161018083525512130589201446.jpg","goods_name":"甜橙晶透亮颜矿物面膜7片","id":"921","is_allow_credit":false,"is_coupon_allowed":true,"market_price":99,"sales_volume":5430,"shop_price":79,"sort":0},{"efficacy":"净透补水 润泽提亮","goods_img":"https://image.yunifang.com/yunifang/images/goods/1189/goods_img/17033018357451202108009476.jpg","goods_name":"清透水嫩亮颜黑面膜套装21片（买就送清爽平衡矿物蚕丝7片）","id":"1189","is_allow_credit":false,"is_coupon_allowed":true,"market_price":297,"sales_volume":5407,"shop_price":99.9,"sort":0},{"efficacy":"清透补水 滋润修护","goods_img":"http://image.hmeili.com/yunifang/images/goods/557/goods_img/160819094421515862876125825.jpg","goods_name":"水润清透养肤面膜套装42片","id":"557","is_allow_credit":false,"is_coupon_allowed":true,"market_price":634,"sales_volume":5237,"shop_price":179.9,"sort":0},{"efficacy":"补水控油 清爽保湿","goods_img":"http://image.hmeili.com/yunifang/images/goods/163/goods_img/160819090788311035481315778.jpg","goods_name":"清爽平衡矿物蚕丝面膜7片","id":"163","is_allow_credit":false,"is_coupon_allowed":true,"market_price":99,"sales_volume":4753,"shop_price":79,"sort":0},{"efficacy":"洁净控油 强劲清爽","goods_img":"http://image.hmeili.com/yunifang/images/goods/868/goods_img/1608191430564693145258305.jpg","goods_name":"男士黑茶清爽矿物面膜  今日买5片送5片","id":"868","is_allow_credit":false,"is_coupon_allowed":true,"market_price":150,"sales_volume":4107,"shop_price":49.9,"sort":0},{"efficacy":"清洁净透 以白养白","goods_img":"http://image.hmeili.com/yunifang/images/goods/534/goods_img/160819094245115265661305031.jpg","goods_name":"净透美白矿物蚕丝面膜套装28片","id":"534","is_allow_credit":false,"is_coupon_allowed":true,"market_price":396,"sales_volume":3908,"shop_price":139.9,"sort":0},{"efficacy":"提拉紧致 补水保湿","goods_img":"http://image.hmeili.com/yunifang/images/goods/81/goods_img/160819085711519851862212059.jpg","goods_name":"人参矿物蚕丝面膜7片","id":"81","is_allow_credit":false,"is_coupon_allowed":true,"market_price":109,"sales_volume":3799,"shop_price":89,"sort":0},{"efficacy":"清洁焕彩 净透亮肤","goods_img":"http://image.hmeili.com/yunifang/images/goods/334/goods_img/160819092848420294792181143.jpg","goods_name":"清爽平衡矿物蚕丝面膜黑面膜7片","id":"334","is_allow_credit":false,"is_coupon_allowed":true,"market_price":99,"sales_volume":3775,"shop_price":79,"sort":0},{"efficacy":"清洁控油 补水提亮","goods_img":"http://image.hmeili.com/yunifang/images/goods/1251/goods_img/170104095895513706519167911.jpg","goods_name":"水果缤纷面膜超值装35片","id":"1251","is_allow_credit":false,"is_coupon_allowed":true,"market_price":495,"sales_volume":3697,"shop_price":169.9,"sort":0},{"efficacy":"平衡水油 净彻亮肤","goods_img":"http://image.hmeili.com/yunifang/images/goods/920/goods_img/16081217454451202108002265.jpg","goods_name":"法国珍珠藻清透面膜5片","id":"920","is_allow_credit":false,"is_coupon_allowed":true,"market_price":79,"sales_volume":3567,"shop_price":59.9,"sort":0},{"efficacy":"补水保湿  润泽滋养","goods_img":"http://image.hmeili.com/yunifang/images/goods/1234/goods_img/161122183456315727984418108.jpg","goods_name":"热销|樱桃鲜润补水矿物面膜7片","id":"1234","is_allow_credit":false,"is_coupon_allowed":false,"market_price":99,"sales_volume":2578,"shop_price":39.9,"sort":0}]
+     * msg : success
+     */
+
+    private int code;
+    private String msg;
+    private List<DataBean> data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public List<DataBean> getData() {
+        return data;
+    }
+
+    public void setData(List<DataBean> data) {
+        this.data = data;
+    }
+
+    public static class DataBean {
+        /**
+         * efficacy : 镇店之宝 美白爆款
+         * goods_img : https://image.yunifang.com/yunifang/images/goods/121/goods_img/170301091610311632161123479.jpg
+         * goods_name : 镇店之宝丨美白嫩肤面膜7片
+         * id : 121
+         * is_allow_credit : false
+         * is_coupon_allowed : true
+         * market_price : 99.0
+         * sales_volume : 122443
+         * shop_price : 49.9
+         * sort : 0
+         */
+
+        private String efficacy;
+        private String goods_img;
+        private String goods_name;
+        private String id;
+        private boolean is_allow_credit;
+        private boolean is_coupon_allowed;
+        private double market_price;
+        private int sales_volume;
+        private double shop_price;
+        private int sort;
+
+        public String getEfficacy() {
+            return efficacy;
+        }
+
+        public void setEfficacy(String efficacy) {
+            this.efficacy = efficacy;
+        }
+
+        public String getGoods_img() {
+            return goods_img;
+        }
+
+        public void setGoods_img(String goods_img) {
+            this.goods_img = goods_img;
+        }
+
+        public String getGoods_name() {
+            return goods_name;
+        }
+
+        public void setGoods_name(String goods_name) {
+            this.goods_name = goods_name;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public boolean isIs_allow_credit() {
+            return is_allow_credit;
+        }
+
+        public void setIs_allow_credit(boolean is_allow_credit) {
+            this.is_allow_credit = is_allow_credit;
+        }
+
+        public boolean isIs_coupon_allowed() {
+            return is_coupon_allowed;
+        }
+
+        public void setIs_coupon_allowed(boolean is_coupon_allowed) {
+            this.is_coupon_allowed = is_coupon_allowed;
+        }
+
+        public double getMarket_price() {
+            return market_price;
+        }
+
+        public void setMarket_price(double market_price) {
+            this.market_price = market_price;
+        }
+
+        public int getSales_volume() {
+            return sales_volume;
+        }
+
+        public void setSales_volume(int sales_volume) {
+            this.sales_volume = sales_volume;
+        }
+
+        public double getShop_price() {
+            return shop_price;
+        }
+
+        public void setShop_price(double shop_price) {
+            this.shop_price = shop_price;
+        }
+
+        public int getSort() {
+            return sort;
+        }
+
+        public void setSort(int sort) {
+            this.sort = sort;
+        }
+    }
+}
